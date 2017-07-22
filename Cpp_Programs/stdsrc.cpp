f389ec283c84eb5ec3be9290e16a520e18956977
xiazeyu <xiazeyu_2011@126.com>
2017-07-22 22:41:04 +0800

    Add Abs Min Max Bit Function 增加位运算模块

---
+++ Cpp_Programs/stdsrc.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define Inf 0x3f3f3f3f
+#define ll long long
+
+using namespace std;
+
+inline int qmin(int a, int b){return a + ((b - a) & ((b - a) >> 31));}
+inline int qmax(int a, int b){return b - ((b - a) & ((b - a) >> 31));}
+inline int qabs(int a){return ((a + (a >> 31)) ^ (a >> 31));}
+
+int main(){
+	
+	
+	
+	return 0;
+	
+}
