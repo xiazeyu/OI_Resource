ae0994908afc7aeb75f55c107832357fb7421dc0
xiazeyu <xiazeyu_2011@126.com>
2017-07-23 18:17:59 +0800

    Update Programmer Kind code 更新程序员友好方便记忆的代码

--- Cpp_Programs/stdsrc.cpp
+++ Cpp_Programs/stdsrc.cpp
@@ -4,9 +4,22 @@
 
 using namespace std;
 
-inline int qmin(int a, int b){return a + ((b - a) & ((b - a) >> 31));}
-inline int qmax(int a, int b){return b - ((b - a) & ((b - a) >> 31));}
-inline int qabs(int a){return ((a + (a >> 31)) ^ (a >> 31));}
+inline int qmin(int a, int b){
+	int diff = b - a;
+	// b < a : a + (diff & -1)
+	// b > a : a + (diff & 0)
+	return a + (diff & (diff >> 31));
+}
+inline int qmax(int a, int b){
+	int diff = b - a;
+	// b < a : b - (diff & -1)
+	// b > a : b - (diff & 0)
+	return b - (diff & (diff >> 31));
+}
+inline int qabs(int a){
+	int t = (a >> 31);
+	return (a + t) ^ t;
+}
 
 int main(){
 	
